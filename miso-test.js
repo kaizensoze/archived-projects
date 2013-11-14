e91844263a18d6bee186c455c3ed5091aa263b55
Joe Gallo <gallo.j@gmail.com>
2013-11-14 17:19:31 -0500

    groupBy, countBy working movingAverage still failing

--- miso-test.js
+++ miso-test.js
@@ -1,8 +1,8 @@
 
 var Miso = require("miso.dataset");
+var _ = require('lodash');
 // var _ = require("underscore");
 // _.mixin(require("underscore.deferred"));
-
 
 var ds = new Miso.Dataset({
   data: [
@@ -15,13 +15,52 @@ var ds = new Miso.Dataset({
   ]
 });
 
+var ds2 = new Miso.Dataset({
+  data: [
+    {key:"A", value:130000},
+    {key:"B", value:420},
+    {key:"C", value:1000},
+    {key:"D", value:200},
+    {key:"E", value:2900},
+    {key:"F", value:4}
+  ]
+});
+
+// count by
 ds.fetch({ 
   success: function() {
-    var gb = this.countBy('key');
-    gb.each(function(row) {
+    var x = this.countBy('key');
+    x.each(function(row) {
       delete row._id;
       delete row._oids;
       console.log(row);
     });
   }
 });
+console.log();
+
+// group by
+ds.fetch({ 
+  success: function() {
+    var x = this.groupBy('key', ['value']);
+    x.each(function(row) {
+      delete row._id;
+      delete row._oids;
+      console.log(row);
+    });
+  }
+});
+console.log();
+
+// moving average
+ds2.fetch({ 
+  success: function() {
+    var x = this.movingAverage('value');
+    x.each(function(row) {
+      delete row._id;
+      delete row._oids;
+      console.log(row);
+    });
+  }
+});
+console.log();
